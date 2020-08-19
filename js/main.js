36ac34f967ab2c065f833547b63b1205748bca20
Drakonkinst <drakonkinstmc@gmail.com>
2020-08-19 11:33:52 -0700

    Added scripting boilerplate

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+function setup() {
+    console.log("Setup complete!");
+}
+
+function draw() {
+    console.log("Drawing!");
+}
+
 $(document).ready(function() {
-    
+    console.log("Ready!");
 });
